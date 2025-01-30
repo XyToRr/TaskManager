2b55628f342cebee69186bb8c93a290489625400
Co1oR <136855192+Co1oR@users.noreply.github.com>
2025-01-30 18:25:44 +0200

    Зробив ініціалізатію

---
+++ TaskManager/BLL/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class ApplicationContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
