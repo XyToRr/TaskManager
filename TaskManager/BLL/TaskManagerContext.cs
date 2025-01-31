b6f5ddf04dd8d7fd16b854168d45f40a67b7b231
XyToRr <132078529+XyToRr@users.noreply.github.com>
2025-01-31 18:39:54 +0200

    Set up fluentAPI

---
+++ TaskManager/BLL/TaskManagerContext.cs
@@ -0,0 +1,63 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    class TaskManagerContext : DbContext
+    {
+        public DbSet<Repository> Repositories { get; set; }
+        public DbSet<TaskComment> TaskComments { get; set; }
+        public DbSet<TaskModel> TaskModels { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserToRepository> UsersToRepositories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            SetUpTaskModelLinks(modelBuilder);
+            SetUpUserRepositoryManyToMany(modelBuilder);
+        }
+
+        private static void SetUpTaskModelLinks(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TaskModel>()
+                .HasOne(t => t.Repository)
+                .WithMany(r => r.Tasks)
+                .HasForeignKey(t => t.RepositoryId);
+
+            modelBuilder.Entity<TaskModel>()
+                .HasOne(t => t.AssignedUser)
+                .WithMany(u => u.AssignedTasks)
+                .HasForeignKey(t => t.AssignedUserId);
+
+            modelBuilder.Entity<TaskModel>()
+                .HasOne(t => t.CreatedUser)
+                .WithMany(u => u.CreatedTasks)
+                .HasForeignKey(t => t.CreatedUserId);
+
+
+            modelBuilder.Entity<TaskComment>()
+                .HasOne(c => c.Task)
+                .WithMany(t => t.Comments)
+                .HasForeignKey(c => c.TaskId);
+        }
+
+        private static void SetUpUserRepositoryManyToMany(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserToRepository>().HasKey(ur => new { ur.RepositoryId, ur.UserId });
+
+            modelBuilder.Entity<UserToRepository>()
+                .HasOne(ur => ur.User)
+                .WithMany(u => u.Repositories)
+                .HasForeignKey(ur => ur.UserId);
+
+            modelBuilder.Entity<UserToRepository>()
+                .HasOne(ur => ur.Repository)
+                .WithMany(r => r.Users)
+                .HasForeignKey(ur => ur.UserId);
+        }
+    }
+}
