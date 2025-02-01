cd8d10c5c59787be35cf68ee76db06655cda63fc
Co1oR <136855192+Co1oR@users.noreply.github.com>
2025-02-01 15:54:25 +0200

    Додав DI

--- TaskManager/TaskManager/App.xaml.cs
+++ TaskManager/TaskManager/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace TaskManager
 {
@@ -9,6 +10,23 @@ namespace TaskManager
     /// </summary>
     public partial class App : Application
     {
+        public static IServiceProvider ServiceProvider { get; private set; }
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var serviceProvider = new ServiceCollection();
+            ConfigurationService(serviceProvider);
+            ServiceProvider = serviceProvider.BuildServiceProvider();
+
+            var mainWindow = ServiceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigurationService(ServiceCollection services)
+        {
+
+            services.AddTransient<MainWindow>();
+
+        }
     }
 
 }
