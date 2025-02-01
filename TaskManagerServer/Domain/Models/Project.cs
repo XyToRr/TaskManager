d80bc26593db1264f499e954582fce5d488cc632
XyToRr <132078529+XyToRr@users.noreply.github.com>
2025-02-01 16:45:56 +0200

    Переніс моделі на сервер

---
+++ TaskManagerServer/Domain/Models/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public virtual List<TaskModel> Tasks { get; set; } = new();
+        public virtual List<UserToProject> Users { get; set; } = new();
+    }
+}
---
+++ TaskManagerServer/Domain/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string PasswordHash { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public virtual List<UserToProject> Repositories { get; set; } = new();
+        public virtual List<TaskModel> CreatedTasks { get; set; } = new();
+        public virtual List<TaskModel> AssignedTasks { get; set; } = new();
+    }
+}
