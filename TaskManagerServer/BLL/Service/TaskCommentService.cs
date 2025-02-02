52391360e81008fdf8c36ac82fb4416bb2c068de
Co1oR <136855192+Co1oR@users.noreply.github.com>
2025-02-02 21:39:07 +0200

    Зробив сервіси на сервері

---
+++ TaskManagerServer/BLL/Service/TaskCommentService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using DLL.Repositories;
+using Domain.Models;
+
+namespace BLL.Service
+{
+    public class TaskCommentService : IService<TaskComment>
+    {
+        public IRepository<TaskComment> taskCommentRepository;
+
+        public TaskCommentService(IRepository<TaskComment> taskCommentRepository) { this.taskCommentRepository = taskCommentRepository; }
+        public IQueryable<TaskComment> GetAll() => taskCommentRepository.GetAll();
+        public IQueryable<TaskComment> GetByCondition(Expression<Func<TaskComment, bool>> predicate) => taskCommentRepository.GetByCondition(predicate);
+        public async Task AddAsync(TaskComment newEntity) => await taskCommentRepository.AddAsync(newEntity);
+        public async Task DeleteAsync(TaskComment comment) => await taskCommentRepository.DeleteAsync(comment.Id);
+        public async Task UpdateAsync(TaskComment newEntity, int oldId) => await taskCommentRepository.UpdateAsync(newEntity, oldId);
+    }
+}
