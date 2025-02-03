aadae57a315150354deb4712d6fd46ffed558036
XyToRr <132078529+XyToRr@users.noreply.github.com>
2025-02-03 17:24:05 +0200

    Додав основу для логіки сервера

---
+++ TaskManagerServer/BLL/Service/ClientHandler.cs
@@ -0,0 +1,81 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace BLL.Service
+{
+    public class ClientHandler
+    {
+        private TcpClient client;
+        private TaskManagerServer server;
+
+        private StreamReader reader;
+        private StreamWriter writer;
+        public ClientHandler(TcpClient client, TaskManagerServer server) 
+        {
+            this.client = client;
+            this.server = server;
+
+            var stream = this.client.GetStream();
+            reader = new StreamReader(stream);
+            writer = new StreamWriter(stream) { AutoFlush = true };
+        }
+
+        public async Task HandleClientAsync()
+        {
+            while (true)
+            {
+                try
+                {
+                    var messageJson = await reader.ReadLineAsync();
+                    if (messageJson == null)
+                        break;
+
+                    var message = JsonSerializer.Deserialize<Message>(messageJson);
+                    if(message != null)
+                    {
+                        HandleMessage(message);
+                    }
+                }
+                catch (Exception ex) 
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+
+        private void HandleMessage(Message message)
+        {
+            switch (message.MessageType)
+            {
+                case MessageType.RegisterRequest:
+                    break;
+                case MessageType.LoginRequest:
+                    break;
+                case MessageType.RoleChange:
+                    break;
+                case MessageType.ProjectCreationRequest:
+                    break;
+                case MessageType.TaskCreationRequest:
+                    break;
+                case MessageType.FindUser:
+                    break;
+                case MessageType.TaskAssignment:
+                    break;
+                case MessageType.TaskCompletion:
+                    break;
+                case MessageType.TaskStateUpdate:
+                    break;
+                case MessageType.AddUserToProject:
+                    break;
+            }
+        }
+    }
+}
